7c93464e1b28dd1519a2d39d31730cfbf5d3f36c
Sandip Punalkar <sandippunalkar9@gmail.com>
2023-07-20 16:44:47 +0530

    Implement Searching.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,11 +4,14 @@ import React from "react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface NavBarProps {
+  onSearch: (searchText: string) => void;
+}
+const NavBar = ({ onSearch }: NavBarProps) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
